08fd55b772a71bb3688c6d6f295ecc4754ef0910
ZiadHegazy99 <ziad.hegazy@student.guc.edu.eg>
2024-07-17 22:35:48 +0300

    last

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,7 +3,7 @@ import './ProductList.css'
 import CartItem from './CartItem';
 import { useDispatch, useSelector } from 'react-redux';
 import { addItem } from './CreateSlice';
-function ProductList() {
+function ProductList(props) {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [cart, setCart] = useState([]); // State to store the items added to the cart
@@ -270,9 +270,9 @@ const handlePlantsClick = (e) => {
         <div>
              <div className="navbar" style={styleObj}>
             <div className="tag">
-               <div className="luxury">
+               <div style={{cursor:"pointer"}} onClick={props.toLanding} className="luxury">
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-               <a href="/e-plantShopping" style={{textDecoration:'none'}}>
+               <a   style={{textDecoration:'none'}}>
                         <div>
                     <h3 style={{color:'white'}}>Ziad's Plants</h3>
                     <i style={{color:'white'}}>Where Green Meets Serenity</i>
